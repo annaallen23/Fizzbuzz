2eb12564819b21183e37e9d1170ecf3a9cbd3f14
Anna.Allen <anna.allen@AnnaAllens-MacBook-Pro.local>
2019-03-11 17:00:14 +0000

    user choice

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,35 +5,40 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Please input a number between 1 & 300");
+        System.out.println("Please input a list of 6 numbers");
 
         Scanner scanner = new Scanner(System.in);
-        int input = scanner.nextInt();
+        int fizzNumber = scanner.nextInt();
+        int fezzNumber = scanner.nextInt();
+        int buzzNumber = scanner.nextInt();
+        int bangNumber = scanner.nextInt();
+        int bongNumber = scanner.nextInt();
+        int revNumber = scanner.nextInt();
 
-        for (int x = 1; x <= input; x++) {
+        for (int x = 0; x <= 100; x++) {
             ArrayList<String> string = new ArrayList<>();
 
-            if (x % 3 == 0) {
+            if (x % fizzNumber == 0) {
                 string.add("Fizz");
             }
-            if (x % 13 == 0) {
+            if (x % fezzNumber == 0) {
                 string.add("Fezz");
             }
-            if (x % 5 == 0) {
+            if (x % buzzNumber == 0) {
                 string.add("Buzz");
             }
-            if (x % 7 == 0) {
+            if (x % bangNumber == 0) {
                 string.add("Bang");
             }
-            if (x % 11 == 0) {
+            if (x % bongNumber == 0) {
                 string.clear();
                 string.add("Bong");
             }
-            if (x % 11 == 0 && x % 13 == 0) {
+            if (x % bongNumber == 0 && x % fezzNumber == 0) {
                 string.clear();
                 string.add("FezzBong");
             }
-            if (x % 17 == 0) {
+            if (x % revNumber == 0) {
                 Collections.reverse(string);
             }
             if (string.size() > 0) {
